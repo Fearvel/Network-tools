3a99c2114b0c54e38cd169dad72b38e5f9f464a6
Andreas Schreiner <schreiner.andreas@outlook.com>
2018-10-14 19:37:17 +0200

    2.3.0.0

---
+++ de.fearvel.net/WebRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace de.fearvel.net
+{
+   public static class WebRequest
+    {
+        public static MemoryStream Download(string url)
+        {
+            MemoryStream ms;
+                WebClient client = new WebClient();
+                try
+                {
+                   ms = new MemoryStream(client.DownloadData(url));
+                }
+                finally
+                {
+                    client.Dispose();
+                }
+            
+            return ms;
+        }
+
+        public static void DownloadToFile(string url, string path)
+        {
+            var ms = Download(url);
+            var f = File.OpenWrite(path);
+            ms.CopyTo(f);
+            ms.Close();
+            f.Close();
+        }
+    }
+}
